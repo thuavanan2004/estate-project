4595113752127a3fdc7ef050f79e2be4134a37ea
thuavanan2004 <thuavanan2004@gmail.com>
2025-03-05 11:12:07 +0700

    init project

---
+++ estate-project/src/main/java/vn/devdynamo/estate_project/EstateProjectApplication.java
@@ -0,0 +1,13 @@
+package vn.devdynamo.estate_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstateProjectApplication.class, args);
+	}
+
+}
---
+++ estate-project/src/test/java/vn/devdynamo/estate_project/EstateProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.devdynamo.estate_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstateProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
